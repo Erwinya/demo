2def5ba75428ff04b92f16452b3f97026acaa540
erwinya <chkilincer@gmail.com>
2025-06-08 14:26:26 +0300

    [LR-2] Username unique features implemented

--- src/main/java/com/example/demo/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/serviceImpl/UserServiceImpl.java
@@ -6,7 +6,10 @@ import com.example.demo.mapper.UserMapper;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @AllArgsConstructor
@@ -16,14 +19,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String addNewUser(UserDTO userDTO) {
+        if (userRepository.findByUsername(userDTO.getUsername()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already exists!");
+        }
         User user = UserMapper.toEntity(userDTO);
         if (user == null) {
-            throw new IllegalArgumentException("User is not valid");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User is not valid");
         }
-        if (userRepository.findByUsername(userDTO.getUsername()).isPresent()) {
-        throw new IllegalArgumentException("Bu kullanıcı adı zaten mevcut!");
-    }    
-        userRepository.save(user);
+        try {
+            userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username must be unique!");
+        }
         return "User added successfully!";
     }
     //asking to serdar for Optional function
